7d572c8f9d121c0fd3365ccdf1102179e8da8bc0
Antony2408 <antony.herrera@unmsm.edu.pe>
2025-07-09 09:55:37 -0500

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'django-insecure-+67_t3f%x62bg$s!(bmq@wiqq+xnpm$h8!1q2()gxl6&m(zxcs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
