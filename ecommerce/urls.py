c9a139d4d8894dc38e6769d064bbd77324a81400
Antony2408 <antony.herrera@unmsm.edu.pe>
2025-07-11 14:13:39 -0500

    Update urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,9 +1,7 @@
+# ecommerce/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework.permissions import AllowAny
@@ -21,10 +19,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('users.urls')),  # Rutas de autenticación
+    path('api/auth/', include('users.urls')),  # Asegúrate de que el archivo users/urls.py exista
     path('api/products/', include('products.urls')),
-    path('api/orders/', include('orders.urls')),  # Rutas de órdenes   
-    # Ruta para la documentación interactiva
+    path('api/orders/', include('orders.urls')),  # Rutas de órdenes
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    
 ]
