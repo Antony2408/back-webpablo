37c14d7e4bbdefc0e8cbbc74159920ec75ce7c07
Antony2408 <antony.herrera@unmsm.edu.pe>
2025-07-11 12:30:34 -0500

    Update views.py

--- products/views.py
+++ products/views.py
@@ -1,12 +1,15 @@
 from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
+from rest_framework.permissions import AllowAny  # Importamos AllowAny para permitir acceso público
 
 class ProductListCreateView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [AllowAny]  # Permite acceso sin autenticación
 
 class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [AllowAny]  # Permite acceso sin autenticación
 
