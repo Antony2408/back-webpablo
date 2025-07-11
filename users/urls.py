380e2127289274d2c833df142ecadd046b8cbe1c
Antony2408 <antony.herrera@unmsm.edu.pe>
2025-07-11 13:56:16 -0500

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,11 @@
-# urls.py
+# users/urls.py
 from django.urls import path
 from .views import CustomTokenObtainPairView, RegisterView, UserListView  
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
-    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('register/', RegisterView.as_view(), name='register'),
-    path('users/', UserListView.as_view(), name='user_list'),  # Acceso público a usuarios
+    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),  # Ruta para login con JWT
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # Ruta para refrescar el token
+    path('register/', RegisterView.as_view(), name='register'),  # Ruta para registrar un nuevo usuario
+    path('users/', UserListView.as_view(), name='user_list'),  # Acceso público a los usuarios
 ]
